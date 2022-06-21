f096f1b380dc880029657e336749d8312043f69f
Ahahaaaa <kwk88mmflak@gmail.com>
2022-06-21 15:01:25 +0900

    更新

---
+++ data.php
@@ -0,0 +1,36 @@
+<?php
+    if(isset($_POST['name'])) {
+        //名前が送信されたら以下の処理を行う
+        //この部分は変更してもいい
+        
+        //「予約フォーム」からの情報をそれぞれ変数に格納しておく↓
+
+$name=htmlspecialchars($_POST["name"], ENT_QUOTES);
+$number=htmlspecialchars($_POST["number"], ENT_QUOTES);
+$member=htmlspecialchars($_POST["member"], ENT_QUOTES);
+$day=htmlspecialchars($_POST["day"], ENT_QUOTES);
+        
+        //「予約フォーム」からの情報をそれぞれ変数に格納しておく↑
+
+        
+$dsn="mysql:host=ホスト名;dbname=データベース名;charset=utf8";
+$user="ユーザー名";
+$pass="パスワード";
+           
+    
+    try{
+        
+$db = new PDO($dsn,$user,$pass);
+$db->query("INSERT INTO テーブル名 (ban,name,number,member,day)
+            VALUES (NULL,'$name','$number','$member','$day')");
+        
+    }catch (Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
+              
+        header("Location: reservation_form.php");
+        //"reservation_form.php（予約フォームがあったページ）"に戻る
+        exit;
+    
+    }
+?>
